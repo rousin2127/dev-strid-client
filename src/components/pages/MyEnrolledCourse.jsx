43d90d5747ebd1ea3510bef00aee5526d2396b41
rousin2127 <rousinmd@gmail.com>
2025-11-12 14:34:53 +0600

    create boylarplate for client side

---
+++ src/components/pages/MyEnrolledCourse.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyEnrolledCourse = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyEnrolledCourse;
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import RootLayout from './layout/RootLayout'
+import Home from './components/Home'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    Component: RootLayout,
+    children:[
+      {
+        index: true,
+        Component: Home
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
